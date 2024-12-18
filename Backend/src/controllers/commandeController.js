09a5c5ff71a4a110a089682331a8f7dca40bfbc3
IliasKhms <152298532+IliasKhms@users.noreply.github.com>
2024-12-18 21:38:08 +0100

    creation du controller

---
+++ Backend/src/controllers/commandeController.js
@@ -0,0 +1,12 @@
+//Ici on met en place le CRUD des commandes : les plus important sont l'Update,Creat et Read
+/*Il existe 3 type d'update : 
+- updateStatus et updatePayment : permet de changer le statut et le paiement de la commande, ce qui va nous permettre de gerer l'affichage des commandes sur l'écran TV en front
+- updateCommande : permet de changer les informations de la commande
+*/
+
+const Commande = require('../models/commande');
+
+
+
+
+
